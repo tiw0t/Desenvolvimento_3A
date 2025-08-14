2f5f5d539079cf93feb171e35d03b3d0d30b441e
Timóteo da Silva Bastos <timoteodasilvabastos15@gmail.com>
2025-08-14 13:47:18 -0300

    foi feito o livro_controller.py, parcialmente completo

---
+++ introd_MVC/models/livro.py
@@ -0,0 +1,10 @@
+class Livro:
+    def __init__(self, id, titulo, autor, ano, isbn):
+        self.id = id
+        self.titulo = titulo
+        self.autor = autor
+        self.ano = ano 
+        self.isbn = isbn
+
+    def __str__(self):
+        return f"Livro(ID: {self.id}, Título: {self.titulo}, Autor: {self.autor}, Ano: {self.ano}, ISBN: {self.isbn})"
